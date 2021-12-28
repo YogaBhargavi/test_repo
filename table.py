b5efcc40b35ceaed6c34eca9e3f5775833a93ad7
Yoga Bhargavi Ponugupati -X (yponugup - ARICENT TECHNOLOGIES HOLDINGS LIMITED at Cisco) <yponugup@cisco.com>
2021-12-28 10:57:07 +0530

    Adding the file

---
+++ table.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 16 16:03:10 2021
+
+@author: yponugup
+"""
+
+n = int(input("Enter n value"))
+for i in range(1,11):
+    print(n, 'X', i, '=', n*i)
+    
